ac42a41b9ec82ed37ae7a239014e58521c06c2d5
Ryan Koh <ryan_koh@berkeley.edu>
2021-02-17 21:00:05 -0800

    stamps and penalty and stuffs

--- shepherd/static/scoreboard.js
+++ shepherd/static/scoreboard.js
@@ -223,3 +223,10 @@ function setStartTime(start_time) {
 
   stageTimerStart(curr_time - start_time)
 }
+
+/* FOR TESTING: */
+// var jq = document.createElement('script');
+// jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js";
+// document.getElementsByTagName('head')[0].appendChild(jq);
+// // ... give time for script to load, then type (or see below for non wait option)
+// jQuery.noConflict();
