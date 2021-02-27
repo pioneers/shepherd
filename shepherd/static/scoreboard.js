1c42afa452907f834add676cf6e871d0b5b99eba
Alex Wong <alexwongapps@gmail.com>
2021-02-27 15:03:49 -0800

    Scoreboard wrapping up
    Co-authored-by: akshitdewan <akshitdewan@users.noreply.github.com>

--- shepherd/static/scoreboard.js
+++ shepherd/static/scoreboard.js
@@ -5,7 +5,7 @@ var globaltime = 0;
 var startTime = 0;
 
 socket.on('connect', (data) => {
-    socket.emit('join', 'scoreboard');
+  socket.emit('join', 'scoreboard');
 });
 
 socket.on('team', (match_info) => {
@@ -13,12 +13,12 @@ socket.on('team', (match_info) => {
   match_info = JSON.parse(match_info)
   team_name = match_info.team_name
   team_num = match_info.team_num
-  nextTeam(team_name, team_num)
+  updateTeam(team_name, team_num)
 })
 
 socket.on('stage_timer_start', (secondsInStage) => {
-    time = JSON.parse(secondsInStage).time
-    stageTimerStart(time)
+  time = JSON.parse(secondsInStage).time
+  stageTimerStart(time)
 })
 
 // STAGE{stage, start_time}
@@ -27,8 +27,14 @@ socket.on('stage', (stage_details) => {
   start_time = JSON.parse(stage_details).start_time
   console.log("got stage header")
   console.log(stage_details)
-  setStageName(stage)
-  setStartTime(start_time)
+  if (stage === "setup") {
+    setTime(0);
+  } else if (stage === "end") {
+    stageTimer = false;
+  } else {
+    setStageName(stage);
+    setStartTime(start_time);
+  }
 })
 
 socket.on("reset_timers", () => {
@@ -41,7 +47,7 @@ socket.on("score", (scores) => {
   console.log("receiving score");
   scores = JSON.parse(scores);
   console.log(`scores are ${JSON.stringify(scores)}`);
-  ({time, penalty, stamp_time} = scores);
+  ({ time, penalty, stamp_time } = scores);
   if (time) {
     setTime(time);
   }
@@ -55,16 +61,15 @@ socket.on("score", (scores) => {
 function setTime(time) {
   stageTimer = false;
   globaltime = time;
-  time = Math.floor(time * 100) / 100;
-  $('#stage-timer').html(Math.floor(time/60) + ":"+ pad(time%60))
+  $('#stage-timer').html(secondsToTimeString(time));
 }
 
 function setStamp(stamp_time) {
-  $('#stamp_time').html(stamp_time);
+  $('#stamp_time').html("-" + secondsToTimeString(-1 * stamp_time));
 }
 
 function setPenalty(penalty) {
-  $('#penalty').html(penalty);
+  $('#penalty').html("+" + secondsToTimeString(penalty));
 }
 
 function setTotal(total) {
@@ -76,7 +81,7 @@ function testScore(score) {
   $('#score').html(score);
 }
 
-function resetTimers(){
+function resetTimers() {
   stageTimer = false;
   timerA = false;
 }
@@ -88,42 +93,39 @@ WAIT = "wait"
 TELEOP = "teleop"
 END = "end"
 
-stage_names = {"setup": "Setup",
-               "auto_wait": "Autonomous Wait", "auto": "Autonomous Period",
-               "teleop": "Teleop Period", "end": "Post-Match"}
+stage_names = {
+  "setup": "Setup",
+  "auto_wait": "Autonomous Wait", "auto": "Autonomous Period",
+  "teleop": "Teleop Period", "end": "Post-Match"
+}
 
 function setStageName(stage) {
   $('#stage').html(stage_names[stage])
 }
 
-function nextTeam(team_name, team_num){
+function updateTeam(team_name, team_num) {
   //set the name and numbers of the school and the match number jk
   $('#team-name').html(team_name)
   $('#team-num').html("Team " + team_num)
-
-  // TODO: Figure out how to reset main timer
 }
 
-function stageTimerStart(currTime) {
+function stageTimerStart(startTime) {
   stageTimer = true;
-  runStageTimer(currTime);
+  runStageTimer(startTime);
 }
 
-function runStageTimer(currTime) {
-  var maxStageTime = 300;
-  if(currTime <= maxStageTime){
-    setTimeout(function() {
-      const currTime = new Date().getTime();
-      const time = (currTime - startTime) / 1000;
-      $('#stage-timer').html(Math.floor(time/60) + ":"+ pad(time%60))
-      if(stageTimer) {
-        runStageTimer(currTime + 1);
-      } else {
-        // supposedly do nothing
-        // runStageTimer(0)
-        return
-      }
-  }, 100);
+function runStageTimer(startTime) {
+  console.log("stage timer is ", stageTimer);
+  if (stageTimer) {
+    const currTime = new Date().getTime() / 1000;
+    console.log(currTime);
+    console.log(startTime);
+    let time = (currTime - startTime);
+    $('#stage-timer').html(secondsToTimeString(time));
+    setTimeout(runStageTimer, 200, startTime);
+  } else {
+    // supposedly do nothing
+    return
   }
 }
 
@@ -131,30 +133,40 @@ function pad(number) {
   return (number < 10 ? '0' : '') + number
 }
 
+function secondsToTimeString(seconds) {
+  if (seconds < 0) {
+    time = Math.floor(-1 * seconds * 100) / 100;
+    return "-" + Math.floor(time / 60) + ":" + pad(Math.floor(time % 60));
+  } else {
+    time = Math.floor(seconds * 100) / 100;
+    return Math.floor(time / 60) + ":" + pad(Math.floor(time % 60));
+  }
+}
+
 function setImageVisible(id, visible) {
   $(id).css("visibility", (visible ? 'visible' : 'hidden'));
 }
 
 function progress(timeleft, timetotal, $element) {
-    var progressBarWidth = timeleft * $element.width() / timetotal;
-    if (timeleft == timetotal) {
-        $element.find('div').animate({ width: progressBarWidth }, 0, 'linear').html(Math.floor(timeleft/60) + ":"+ pad(timeleft%60));
-    } else {
-        $element.find('div').animate({ width: progressBarWidth }, 1000, 'linear').html(Math.floor(timeleft/60) + ":"+ pad(timeleft%60));
-    }
-    if (timeleft > 0) {
-        setTimeout(function() {
-            if(overTimer) {
-              progress(timeleft - 1, timetotal, $element);
-            } else {
-              progress(0, 0, $element)
-            }
-        }, 1000);
-    } else {
-      $element.find('div').animate({ width: 0 }, 1000, 'linear').html("")
-      $('#overdriveText').css('color', 'white');
-      // $('#overdriveText').html("OVERDRIVE!!! " + block + " size!!!");
-    }
+  var progressBarWidth = timeleft * $element.width() / timetotal;
+  if (timeleft == timetotal) {
+    $element.find('div').animate({ width: progressBarWidth }, 0, 'linear').html(Math.floor(timeleft / 60) + ":" + pad(timeleft % 60));
+  } else {
+    $element.find('div').animate({ width: progressBarWidth }, 1000, 'linear').html(Math.floor(timeleft / 60) + ":" + pad(timeleft % 60));
+  }
+  if (timeleft > 0) {
+    setTimeout(function () {
+      if (overTimer) {
+        progress(timeleft - 1, timetotal, $element);
+      } else {
+        progress(0, 0, $element)
+      }
+    }, 1000);
+  } else {
+    $element.find('div').animate({ width: 0 }, 1000, 'linear').html("")
+    $('#overdriveText').css('color', 'white');
+    // $('#overdriveText').html("OVERDRIVE!!! " + block + " size!!!");
+  }
 };
 
 var a = 0
@@ -170,30 +182,30 @@ function draw() {
   // a should depend on the amount of time left
   a++;
   a %= 360;
-  var r = ( a * pi / 180 )
-    , x = Math.sin( r ) * 15000
-  , y = Math.cos( r ) * - 15000
-  , mid = ( a > 180 ) ? 1 : 0
+  var r = (a * pi / 180)
+    , x = Math.sin(r) * 15000
+    , y = Math.cos(r) * - 15000
+    , mid = (a > 180) ? 1 : 0
     , anim =
-        'M 0 0 v -15000 A 15000 15000 1 '
-           + mid + ' 1 '
-           +  x  + ' '
-           +  y  + ' z';
+      'M 0 0 v -15000 A 15000 15000 1 '
+      + mid + ' 1 '
+      + x + ' '
+      + y + ' z';
   //[x,y].forEach(function( d ){
   //  d = Math.round( d * 1e3 ) / 1e3;
   //});
-  $("#loader").attr( 'd', anim );
+  $("#loader").attr('d', anim);
   console.log(counter);
 
   // time left should be calculated using a timer that runs separately
-  if (a % (360 / t) == 0){
+  if (a % (360 / t) == 0) {
     counter -= 1;
     if (counter <= 9) {
       $("#textbox").css("left = '85px';")
     }
     $("#textbox").html(counter);
   }
-  if (a == 0){
+  if (a == 0) {
     return;
   }
   setTimeout(draw, 20); // Redraw
@@ -207,7 +219,7 @@ function runTimer1() {
   launchButtonTimer('.timer1', '.circle_animation1', timerA);
 }
 
-function timer1() { 
+function timer1() {
   /* how long the timer will run (seconds) */
 
   var time = 30;
@@ -215,18 +227,18 @@ function timer1() {
   var i = 1;
 
   /* Need initial run as interval hasn't yet occured... */
-  $('.circle_animation1').css('stroke-dashoffset', initialOffset-(1*(initialOffset/time)));
+  $('.circle_animation1').css('stroke-dashoffset', initialOffset - (1 * (initialOffset / time)));
 
-  var interval = setInterval(function() {
-      $('.timer1').text(time - i);
-      if (i == time||!timerA) {  	
-        clearInterval(interval);
-        $('.timer1').text(30);
-        $('.circle_animation1').css('stroke-dashoffset', '0')
-        return;
-      }
-      $('.circle_animation1').css('stroke-dashoffset', initialOffset-((i+1)*(initialOffset/time)));
-      i++;
+  var interval = setInterval(function () {
+    $('.timer1').text(time - i);
+    if (i == time || !timerA) {
+      clearInterval(interval);
+      $('.timer1').text(30);
+      $('.circle_animation1').css('stroke-dashoffset', '0')
+      return;
+    }
+    $('.circle_animation1').css('stroke-dashoffset', initialOffset - ((i + 1) * (initialOffset / time)));
+    i++;
   }, 1000);
 
 }
@@ -238,11 +250,11 @@ function setStartTime(start_time) {
   //
   // Args:
   // start_time = timestamp sent by Shepherd of when the stage began in seconds
-  start_time = start_time / 1000;
-  var curr_time = new Date().getTime() / 1000;
+  start_time = start_time / 1000; // seconds
 
-  stageTimerStart(Math.floor(curr_time - start_time))
+  stageTimerStart(start_time);
 }
+
 
 /* FOR TESTING: */
 // var jq = document.createElement('script');
