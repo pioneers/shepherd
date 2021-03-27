402c21e0f5f40f1c28c98f8a8069345645c32e74
Akshit Dewan <akshit@berkeley.edu>
2021-03-27 12:45:09 -0400

    ok

--- shepherd/lowcar/devices/Arduino1/Arduino1.cpp
+++ shepherd/lowcar/devices/Arduino1/Arduino1.cpp
@@ -47,6 +47,7 @@ Arduino1::Arduino1() : Device(DeviceType::ARDUINO1, 13) {
 size_t Arduino1::device_read(uint8_t param, uint8_t* data_buf) {
     // put pin value into data_buf and return the state of the switch
     data_buf[0] = (digitalRead(this->pins[param]) == HIGH);
+    this->msngr->lowcar_printf("data_buf[0] is %d", data_buf[0]);
     if (data_buf[0] == true) {
         this->led->slow_blink(3);
     }
