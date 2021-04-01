c4a011a8b5bd53b3a18435abce285875cfe45dd0
Samuel Berkun <sberkun@berkeley.edu>
2021-03-31 20:26:52 -0700

    testing

---
+++ shepherd/student.py
@@ -0,0 +1,2 @@
+
+print("Hello world! I am a student")
--- shepherd/student_code.sh
+++ shepherd/student_code.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # $1 is the IP
-scp pi@$1:/home/pi/runtime/executor/studentcode.py /student.py
-raspberry
+#scp pi@$1:/home/pi/runtime/executor/studentcode.py ./student.py
+#raspberry
+
+echo "Success! (this is for testing only)"
