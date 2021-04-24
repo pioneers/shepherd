5726fe698d532c1641a57a229d040c45e6fa2cf2
Akshit Dewan <akshit@berkeley.edu>
2021-04-24 18:36:08 -0400

    update coding challenge results, 4/24 evening

--- shepherd/challenge_results.py
+++ shepherd/challenge_results.py
@@ -10,4 +10,8 @@ CHALLENGE_RESULTS[35] = [True] * 8
 CHALLENGE_RESULTS[42] = [True, True, True, True, True, False, True, False]
 CHALLENGE_RESULTS[44] = [True, True, False, True, False, False, False, False]
 CHALLENGE_RESULTS[46] = [True] * 8
-
+CHALLENGE_RESULTS[4] = [True] * 8
+CHALLENGE_RESULTS[40] = [False, True, False, True, True, True, True, False]
+CHALLENGE_RESULTS[2] = [True] * 8
+CHALLENGE_RESULTS[15] = [True, False, True, True, True, False, False, False]
+CHALLENGE_RESULTS[10] = [True] * 8
