21ff717ae4e2c1901d571389f049527c0a987cbc
Akshit Dewan <akshit@berkeley.edu>
2021-04-28 23:24:40 -0400

    change 4 --> 6 and pass double trouble

--- shepherd/challenge_results.py
+++ shepherd/challenge_results.py
@@ -4,13 +4,13 @@ results = [False, False, False, False, False, False, False, False]
 
 CHALLENGE_RESULTS = defaultdict(lambda: results)
 CHALLENGE_RESULTS[12] = [True] * 8
-CHALLENGE_RESULTS[26] = [True, True, True, True, True, False, True, True]
+CHALLENGE_RESULTS[26] = [True] * 8
 CHALLENGE_RESULTS[31] = [True] * 8
 CHALLENGE_RESULTS[35] = [True] * 8
 CHALLENGE_RESULTS[42] = [True, True, True, True, True, False, True, False]
 CHALLENGE_RESULTS[44] = [True, True, False, True, False, False, False, False]
 CHALLENGE_RESULTS[46] = [True] * 8
-CHALLENGE_RESULTS[4] = [True] * 8
+CHALLENGE_RESULTS[6] = [True] * 8
 CHALLENGE_RESULTS[40] = [False, True, False, True, True, True, True, False]
 CHALLENGE_RESULTS[2] = [True] * 8
 CHALLENGE_RESULTS[15] = [True, False, True, True, True, False, False, False]
