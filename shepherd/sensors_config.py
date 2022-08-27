1525a170308926ab0919017beff3e405efe12915
Clorox <piepie@piepiepie.pie>
2022-08-27 15:50:01 -0700

    3 button wackamole working

--- shepherd/sensors_config.py
+++ shepherd/sensors_config.py
@@ -5,8 +5,6 @@ from utils import YDL_TARGETS, SHEPHERD_HEADER, SENSOR_HEADER
 
 yc = YDLClient(YDL_TARGETS.SENSORS)
 arduino1 = Arduino(1)
-arduino2 = Arduino(2)
-dummy_arduino = Arduino(123456)
 high = DigitalValue.HIGH.value
 low = DigitalValue.LOW.value
 
@@ -17,35 +15,20 @@ def make_button_handler(id):
             yc.send(SHEPHERD_HEADER.BUTTON_PRESS(id=id))
     return handler
 
-def make_linebreak_handler(id):
-    def handler(state):
-        if state == DigitalValue.LOW.value:
-            print(f"Linebreak {id} was crossed!")
-            yc.send(SHEPHERD_HEADER.BUTTON_PRESS(id=100000+id)) # TODO: add actual linebreak header
-    return handler
-
 lights = [
     OutputPin(arduino1, 3, PinMode.DIGITAL_OUT, initial_value=low), # button lights
     OutputPin(arduino1, 5, PinMode.DIGITAL_OUT, initial_value=low), # corner lights
-    OutputPin(arduino2, 3, PinMode.DIGITAL_OUT, initial_value=low), # button lights
-    OutputPin(arduino2, 5, PinMode.DIGITAL_OUT, initial_value=low), # corner lights
-    OutputPin(arduino1, 7, PinMode.DIGITAL_OUT, initial_value=low), # midline lights
-    OutputPin(arduino2, 7, PinMode.DIGITAL_OUT, initial_value=low), # midline lights
+    OutputPin(arduino1, 7, PinMode.DIGITAL_OUT, initial_value=low), # button lights
 ]
 buttons = [
     InputPin(arduino1, 2, PinMode.DIGITAL_IN, make_button_handler(0)), # button
-    InputPin(arduino2, 2, PinMode.DIGITAL_IN, make_button_handler(1)), # button
-]
-lasers = [
-    OutputPin(dummy_arduino, 2, PinMode.DIGITAL_OUT, initial_value=low), # example laser
-]
-linebreaks = [
-    InputPin(dummy_arduino, 10, PinMode.PULSE_IN, make_linebreak_handler(5)) # example linebreak
+    InputPin(arduino1, 4, PinMode.DIGITAL_IN, make_button_handler(1)), # button
+    InputPin(arduino1, 6, PinMode.DIGITAL_IN, make_button_handler(2)), # button
 ]
 
 start_device_handlers(
     ["/dev/ttyACM" + str(a) for a in range(5)],
-    [arduino1, arduino2]
+    [arduino1]
 )
 
 incoming_headers = {
--- shepherd/whack_a_mole.py
+++ shepherd/whack_a_mole.py
@@ -2,22 +2,29 @@
 import time
 import random
 import queue
+import threading
 from ydl import YDLClient
 from utils import *
 
 
-NUM_BUTTONS = 1
+NUM_BUTTONS = 3
 YC = YDLClient(YDL_TARGETS.SHEPHERD)
+EVENT_QUEUE = queue.Queue()
 
 def turn_all_lights(on):
     head = SENSOR_HEADER.TURN_ON_BUTTON_LIGHT if on else SENSOR_HEADER.TURN_OFF_BUTTON_LIGHT
     for i in range(NUM_BUTTONS):
         YC.send((YDL_TARGETS.SENSORS, head.name, {"id": i}))
 
+def fill_queue():
+    while True:
+        EVENT_QUEUE.put(YC.receive())
+
 def start():
 
     #ydl_send(YDL_TARGETS.SENSORS, SENSOR_HEADER.TURN_ON_LIGHT.name, {"id": 1})
     #print("banana boat")
+    """
     while True:
         turn_all_lights(on=False)
         time.sleep(1)
@@ -25,27 +32,25 @@ def start():
         time.sleep(1)
     """
     turn_all_lights(on=False)
-    event_queue = queue.Queue()
-    ydl_start_read(YDL_TARGETS.SHEPHERD, event_queue)
     score = 0
     while True:
         button = int(random.random()*NUM_BUTTONS)
-        ydl_send(YDL_TARGETS.SENSORS, SENSOR_HEADER.TURN_ON_BUTTON_LIGHT.name, {"id": button})
+        YC.send((YDL_TARGETS.SENSORS, SENSOR_HEADER.TURN_ON_BUTTON_LIGHT.name, {"id": button}))
         
-        while not event_queue.empty(): event_queue.get(True) # clear the queue 
+        while not EVENT_QUEUE.empty(): EVENT_QUEUE.get(True) # clear the queue 
         delay = random.random()
         delay = delay if delay > .4 else .4
         waited = 0
         pressed = False
-        while not event_queue.empty() or (waited < delay and not pressed):
+        while not EVENT_QUEUE.empty() or (waited < delay and not pressed):
             waited += 0.01
             time.sleep(0.01)
             try:
-                message = event_queue.get(False)
+                message = EVENT_QUEUE.get(False)
             except queue.Empty:
                 continue
-            if message[0] == SHEPHERD_HEADER.BUTTON_PRESS.name:
-                if int(message[1]["button"]) == button:
+            if message[1] == SHEPHERD_HEADER.BUTTON_PRESS.name:
+                if int(message[2]["id"]) == button:
                     pressed = True
         if pressed:
             print(f"got {button} in {round(waited,2)} seconds", end=" ")
@@ -56,9 +61,9 @@ def start():
         else:
             print(f":( {button}", end=" ")
         print(f'score: {score}')
-        ydl_send(YDL_TARGETS.SENSORS, SENSOR_HEADER.TURN_ON_BUTTON_LIGHT.name, {"id": button})
+        YC.send((YDL_TARGETS.SENSORS, SENSOR_HEADER.TURN_OFF_BUTTON_LIGHT.name, {"id": button}))
         sleeptime = random.random() + 1
         time.sleep(sleeptime)
-        """
 
+threading.Thread(target=fill_queue, args=(), daemon=True).start()
 start()
