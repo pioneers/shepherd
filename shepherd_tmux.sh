b84c6917b5639ca421486907d506fbf895dee7d5
Samuel Berkun <sberkun@berkeley.edu>
2023-02-01 19:37:38 -0800

    the great renaming

--- shepherd_tmux.sh
+++ shepherd_tmux.sh
@@ -1,16 +1,15 @@
 tmux new-session -d -s Shepherd -n Shepherd
-tmux send-keys -t '=Shepherd:0' 'cd shepherd' Enter
 tmux split-window -h -t '=Shepherd:0'
 tmux split-window -h -t '=Shepherd:0'
 tmux split-window -v -t '=Shepherd:0'
 tmux split-window -v -t '=Shepherd:0.1'
 tmux split-window -v -t '=Shepherd:0.0'
-tmux send-keys -t 'Shepherd:0.0' 'cd shepherd' Enter
-tmux send-keys -t 'Shepherd:0.1' 'cd shepherd' Enter
-tmux send-keys -t 'Shepherd:0.2' 'cd shepherd' Enter
-tmux send-keys -t 'Shepherd:0.3' 'cd shepherd' Enter
-tmux send-keys -t 'Shepherd:0.4' 'cd shepherd' Enter
-tmux send-keys -t 'Shepherd:0.5' 'cd shepherd' Enter
+tmux send-keys -t 'Shepherd:0.0' 'cd src' Enter
+tmux send-keys -t 'Shepherd:0.1' 'cd src' Enter
+tmux send-keys -t 'Shepherd:0.2' 'cd src' Enter
+tmux send-keys -t 'Shepherd:0.3' 'cd src' Enter
+tmux send-keys -t 'Shepherd:0.4' 'cd src' Enter
+tmux send-keys -t 'Shepherd:0.5' 'cd src' Enter
 
 tmux send-keys -t 'Shepherd:0.0' 'python3 shepherd.py' Enter
 tmux send-keys -t 'Shepherd:0.1' 'echo free terminal' Enter
