b8b4e29f25e897ebf93a5e68335eeab443b4abb5
Clorox <piepie@piepiepie.pie>
2024-04-05 21:41:16 -0700

    sensor update

--- src/sensors_config.py
+++ src/sensors_config.py
@@ -14,17 +14,21 @@ YC = Client(YDL_TARGETS.SENSORS)
 yh = Handler()
 
 lights = [
-    OutputPin(arduino1, 10, PinMode.DIGITAL_OUT, initial_value=high),
     OutputPin(arduino1, 9, PinMode.DIGITAL_OUT, initial_value=high),
     OutputPin(arduino1, 7, PinMode.DIGITAL_OUT, initial_value=high),
     OutputPin(arduino1, 5, PinMode.DIGITAL_OUT, initial_value=high),
-    OutputPin(arduino1, 3, PinMode.DIGITAL_OUT, initial_value=high),
-
-    OutputPin(arduino2, 16, PinMode.DIGITAL_OUT, initial_value=high),
+ 
     OutputPin(arduino2, 9, PinMode.DIGITAL_OUT, initial_value=high),
     OutputPin(arduino2, 7, PinMode.DIGITAL_OUT, initial_value=high),
     OutputPin(arduino2, 5, PinMode.DIGITAL_OUT, initial_value=high),
-    OutputPin(arduino2, 3, PinMode.DIGITAL_OUT, initial_value=high)
+
+    OutputPin(arduino3, 9, PinMode.DIGITAL_OUT, initial_value=high),
+    OutputPin(arduino3, 7, PinMode.DIGITAL_OUT, initial_value=high),
+    OutputPin(arduino3, 5, PinMode.DIGITAL_OUT, initial_value=high),
+
+    OutputPin(arduino4, 9, PinMode.DIGITAL_OUT, initial_value=high),
+    OutputPin(arduino4, 7, PinMode.DIGITAL_OUT, initial_value=high),
+    OutputPin(arduino4, 5, PinMode.DIGITAL_OUT, initial_value=high),
 ]
 
 
@@ -37,33 +41,39 @@ def make_button_handler(id):
 
 
 buttons = [
-    InputPin(arduino1, 16, PinMode.DIGITAL_IN, make_button_handler(0)),
-    InputPin(arduino1, 8, PinMode.DIGITAL_IN, make_button_handler(1)),
-    InputPin(arduino1, 6, PinMode.DIGITAL_IN, make_button_handler(2)),
-    InputPin(arduino1, 4, PinMode.DIGITAL_IN, make_button_handler(3)),
-    InputPin(arduino1, 2, PinMode.DIGITAL_IN, make_button_handler(4)),
+    InputPin(arduino1, 8, PinMode.DIGITAL_IN, make_button_handler(0)),
+    InputPin(arduino1, 6, PinMode.DIGITAL_IN, make_button_handler(1)),
+    InputPin(arduino1, 4, PinMode.DIGITAL_IN, make_button_handler(2)),
+    
+    InputPin(arduino2, 8, PinMode.DIGITAL_IN, make_button_handler(3)),
+    InputPin(arduino2, 6, PinMode.DIGITAL_IN, make_button_handler(4)),
+    InputPin(arduino2, 4, PinMode.DIGITAL_IN, make_button_handler(5)),
 
-    InputPin(arduino2, 10, PinMode.DIGITAL_IN, make_button_handler(5)),
-    InputPin(arduino2, 8, PinMode.DIGITAL_IN, make_button_handler(6)),
-    InputPin(arduino2, 6, PinMode.DIGITAL_IN, make_button_handler(7)),
-    InputPin(arduino2, 4, PinMode.DIGITAL_IN, make_button_handler(8)),
-    InputPin(arduino2, 2, PinMode.DIGITAL_IN, make_button_handler(9))
+    InputPin(arduino3, 8, PinMode.DIGITAL_IN, make_button_handler(6)),
+    InputPin(arduino3, 6, PinMode.DIGITAL_IN, make_button_handler(7)),
+    InputPin(arduino3, 4, PinMode.DIGITAL_IN, make_button_handler(8)),
+    
+    InputPin(arduino4, 8, PinMode.DIGITAL_IN, make_button_handler(9)),
+    InputPin(arduino4, 6, PinMode.DIGITAL_IN, make_button_handler(10)),
+    InputPin(arduino4, 4, PinMode.DIGITAL_IN, make_button_handler(11)),
 ]
 # color_sensor = InputPin(arduino1, 123, PinMode.PULSE_IN, banana)
 
 start_device_handlers(
     ["/dev/ttyACM" + str(a) for a in range(3)],  # CHANGE THIS IF NOT ON LINUX
-    [arduino1, arduino2, arduino3, arduino4, ]
+    [arduino1, arduino2, arduino3, arduino4]
 )
 
 
 @yh.on(SENSOR_HEADER.TURN_ON_BUTTON_LIGHT)
 def turn_on_button_light(id):
+    print("turn the light on")
     lights[id].set_state(high)
 
 
 @yh.on(SENSOR_HEADER.TURN_OFF_BUTTON_LIGHT)
 def turn_off_button_light(id):
+    print("turn the light off")
     lights[id].set_state(low)
 
 
--- src/test_sensors_config.py
+++ src/test_sensors_config.py
@@ -5,19 +5,36 @@ low = DigitalValue.LOW.value
 high = DigitalValue.HIGH.value
 arduino1 = Arduino(1)
 arduino2 = Arduino(2)
+arduino3 = Arduino(3)
+arduino4 = Arduino(4)
+
 
 lights = [
-    OutputPin(arduino1, 10, PinMode.DIGITAL_OUT, initial_value=high),
     OutputPin(arduino1, 9, PinMode.DIGITAL_OUT, initial_value=high),
     OutputPin(arduino1, 7, PinMode.DIGITAL_OUT, initial_value=high),
     OutputPin(arduino1, 5, PinMode.DIGITAL_OUT, initial_value=high),
-    OutputPin(arduino1, 3, PinMode.DIGITAL_OUT, initial_value=high),
-
-    OutputPin(arduino2, 16, PinMode.DIGITAL_OUT, initial_value=high),
+ 
     OutputPin(arduino2, 9, PinMode.DIGITAL_OUT, initial_value=high),
     OutputPin(arduino2, 7, PinMode.DIGITAL_OUT, initial_value=high),
     OutputPin(arduino2, 5, PinMode.DIGITAL_OUT, initial_value=high),
-    OutputPin(arduino2, 3, PinMode.DIGITAL_OUT, initial_value=high)
+
+    OutputPin(arduino3, 9, PinMode.DIGITAL_OUT, initial_value=high),
+    OutputPin(arduino3, 7, PinMode.DIGITAL_OUT, initial_value=high),
+    OutputPin(arduino3, 5, PinMode.DIGITAL_OUT, initial_value=high),
+
+    OutputPin(arduino4, 9, PinMode.DIGITAL_OUT, initial_value=high),
+    OutputPin(arduino4, 7, PinMode.DIGITAL_OUT, initial_value=high),
+    OutputPin(arduino4, 5, PinMode.DIGITAL_OUT, initial_value=high),
+
+
+    # OutputPin(arduino1, 10, PinMode.DIGITAL_OUT, initial_value=high),
+    # OutputPin(arduino1, 3, PinMode.DIGITAL_OUT, initial_value=high),
+
+    # OutputPin(arduino2, 16, PinMode.DIGITAL_OUT, initial_value=high),
+    # OutputPin(arduino2, 9, PinMode.DIGITAL_OUT, initial_value=high),
+    # OutputPin(arduino2, 7, PinMode.DIGITAL_OUT, initial_value=high),
+    # OutputPin(arduino2, 5, PinMode.DIGITAL_OUT, initial_value=high),
+    # OutputPin(arduino2, 3, PinMode.DIGITAL_OUT, initial_value=high)
 ]
 
 
@@ -27,23 +44,37 @@ def make_button_handler(id):
     return handler
 
 buttons = [
-    InputPin(arduino1, 16, PinMode.DIGITAL_IN, make_button_handler(0)),
-    InputPin(arduino1, 8, PinMode.DIGITAL_IN, make_button_handler(1)),
-    InputPin(arduino1, 6, PinMode.DIGITAL_IN, make_button_handler(2)),
-    InputPin(arduino1, 4, PinMode.DIGITAL_IN, make_button_handler(3)),
-    InputPin(arduino1, 2, PinMode.DIGITAL_IN, make_button_handler(4)),
+    InputPin(arduino1, 8, PinMode.DIGITAL_IN, make_button_handler(0)),
+    InputPin(arduino1, 6, PinMode.DIGITAL_IN, make_button_handler(1)),
+    InputPin(arduino1, 4, PinMode.DIGITAL_IN, make_button_handler(2)),
+    
+    InputPin(arduino2, 8, PinMode.DIGITAL_IN, make_button_handler(0)),
+    InputPin(arduino2, 6, PinMode.DIGITAL_IN, make_button_handler(1)),
+    InputPin(arduino2, 4, PinMode.DIGITAL_IN, make_button_handler(2)),
 
-    InputPin(arduino2, 10, PinMode.DIGITAL_IN, make_button_handler(5)),
-    InputPin(arduino2, 8, PinMode.DIGITAL_IN, make_button_handler(6)),
-    InputPin(arduino2, 6, PinMode.DIGITAL_IN, make_button_handler(7)),
-    InputPin(arduino2, 4, PinMode.DIGITAL_IN, make_button_handler(8)),
-    InputPin(arduino2, 2, PinMode.DIGITAL_IN, make_button_handler(9))
+    InputPin(arduino3, 8, PinMode.DIGITAL_IN, make_button_handler(0)),
+    InputPin(arduino3, 6, PinMode.DIGITAL_IN, make_button_handler(1)),
+    InputPin(arduino3, 4, PinMode.DIGITAL_IN, make_button_handler(2)),
+
+    InputPin(arduino4, 8, PinMode.DIGITAL_IN, make_button_handler(0)),
+    InputPin(arduino4, 6, PinMode.DIGITAL_IN, make_button_handler(1)),
+    InputPin(arduino4, 4, PinMode.DIGITAL_IN, make_button_handler(2)),
+    
+    
+    # InputPin(arduino1, 4, PinMode.DIGITAL_IN, make_button_handler(3)),
+    # InputPin(arduino1, 2, PinMode.DIGITAL_IN, make_button_handler(4)),
+
+    # InputPin(arduino2, 10, PinMode.DIGITAL_IN, make_button_handler(5)),
+    # InputPin(arduino2, 8, PinMode.DIGITAL_IN, make_button_handler(6)),
+    # InputPin(arduino2, 6, PinMode.DIGITAL_IN, make_button_handler(7)),
+    # InputPin(arduino2, 4, PinMode.DIGITAL_IN, make_button_handler(8)),
+    # InputPin(arduino2, 2, PinMode.DIGITAL_IN, make_button_handler(9))
 ]
 # color_sensor = InputPin(arduino1, 123, PinMode.PULSE_IN, banana)
 
 start_device_handlers(
     ["/dev/ttyACM" + str(a) for a in range(5)], # CHANGE THIS IF NOT ON LINUX
-    [arduino1, arduino2]
+    [arduino1, arduino2, arduino3, arduino4]
 )
 
 while True:
