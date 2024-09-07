6e1652718090adf512ccfd56b6ffc7aeca8dda01
n1qqq <n1qqq@foxmail.com>
2024-09-08 06:14:54 +0800

    Updated the google doc file into a new 2024 file

--- src/utils.py
+++ src/utils.py
@@ -407,7 +407,7 @@ UI_PAGES = {
 class CONSTANTS():
     BLIZZARD_WARNING_TIME = 170
     CSV_FILE_NAME = "sheets/Shepherd Evergreen Database - Match Database.csv"
-    SPREADSHEET_ID = "1cgt5r4ABchCqxiZYUUJc7kKLSZTQKHQB_9SHeZo2Gbg"
+    SPREADSHEET_ID = "1DWPiFa7ZJlbJ9muitG8jtjwHK-D-FKh2LDJE36SnRqY"
     UI_PASSWORD_HASH = "44590c963be2a79f52c07f7a7572b3907bf5bb180d993bd31aab510d29bbfbd3"
 
 
