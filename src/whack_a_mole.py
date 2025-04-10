c923977d259952105a26d758c462549cf8a548a0
Gantamir Gankhuyag <gantasbeast@visitor-10-57-208-37.wifi.berkeley.edu>
2025-04-09 20:37:22 -0700

    Added sail_task function, commented out all the cheat codes in the whack a mole, deleted arduino3, 4 in sensor_config.py

--- src/whack_a_mole.py
+++ src/whack_a_mole.py
@@ -8,7 +8,7 @@ from utils import *
 
 
 REQUIREMENT = 5
-NUM_BUTTONS = 6
+NUM_BUTTONS = 5
 YC = Client(YDL_TARGETS.SHEPHERD)
 BLUE_QUEUE = queue.Queue()
 GOLD_QUEUE = queue.Queue()
@@ -36,12 +36,12 @@ def send_security_breach_score(alliance, done):
     YC.send((SHEPHERD_HEADER.UPDATE_SECURITY_BREACH_SCORE(alliance, done)))
 
 
-def send_cheat_code_score(alliance, CHEAT_CODE_DONE):
-    YC.send((SHEPHERD_HEADER.UPDATE_CHEAT_CODE_SCORE(alliance, CHEAT_CODE_DONE)))
+# def send_cheat_code_score(alliance, CHEAT_CODE_DONE):
+#     YC.send((SHEPHERD_HEADER.UPDATE_CHEAT_CODE_SCORE(alliance, CHEAT_CODE_DONE)))
 
 
-def set_cheat_code(alliance, CHEAT_CODE):
-    YC.send((SHEPHERD_HEADER.SET_CHEAT_CODE(alliance, CHEAT_CODE)))
+# def set_cheat_code(alliance, CHEAT_CODE):
+#     YC.send((SHEPHERD_HEADER.SET_CHEAT_CODE(alliance, CHEAT_CODE)))
 
 
 def check_live_coding(alliance):
@@ -64,12 +64,12 @@ def fill_queue():
 #
 
 
-def make_cheat_code(alliance):
-    CHEAT_CODE = [random.randint(0, 5) for _ in range(10)]
-    set_cheat_code(alliance, CHEAT_CODE)
-    if alliance == ALLIANCE_COLOR.GOLD:
-        CHEAT_CODE = [i + 5 for i in CHEAT_CODE]
-    return CHEAT_CODE
+# def make_cheat_code(alliance):
+#     CHEAT_CODE = [random.randint(0, 5) for _ in range(10)]
+#     set_cheat_code(alliance, CHEAT_CODE)
+#     if alliance == ALLIANCE_COLOR.GOLD:
+#         CHEAT_CODE = [i + 5 for i in CHEAT_CODE]
+#     return CHEAT_CODE
 
 
 def celebrate(alliance):
@@ -101,12 +101,12 @@ def whack_a_mole_start(alliance):
     turn_all_lights(alliance, on=False)
 
     mole_press_count = 0
-    CHEAT_CODE_DONE = 0
+    # CHEAT_CODE_DONE = 0
     MOLE_PRESS_DONE = False
     EVENT_QUEUE = BLUE_QUEUE if alliance == ALLIANCE_COLOR.BLUE else GOLD_QUEUE
-    CHEAT_CODE = make_cheat_code(alliance)
-    CHEAT_CODE_1, CHEAT_CODE_2 = CHEAT_CODE[:5], CHEAT_CODE[5:]
-    CHEAT_CODE_COPY_1, CHEAT_CODE_COPY_2 = copy.deepcopy(CHEAT_CODE_1), copy.deepcopy(CHEAT_CODE_2)
+    # CHEAT_CODE = make_cheat_code(alliance)
+    # CHEAT_CODE_1, CHEAT_CODE_2 = CHEAT_CODE[:5], CHEAT_CODE[5:]
+    # CHEAT_CODE_COPY_1, CHEAT_CODE_COPY_2 = copy.deepcopy(CHEAT_CODE_1), copy.deepcopy(CHEAT_CODE_2)
 
     """
         When the function starts
@@ -144,12 +144,12 @@ def whack_a_mole_start(alliance):
 
             if message[1] in ["start_next_stage", "setup_match"]:
                 turn_all_lights(alliance, on=False)
-                CHEAT_CODE_DONE = 0
+                # CHEAT_CODE_DONE = 0
                 mole_press_count = 0
                 MOLE_PRESS_DONE = False
-                CHEAT_CODE = make_cheat_code(alliance)
-                CHEAT_CODE_1, CHEAT_CODE_2 = CHEAT_CODE[:5], CHEAT_CODE[5:]
-                CHEAT_CODE_COPY_1, CHEAT_CODE_COPY_2 = copy.deepcopy(CHEAT_CODE_1), copy.deepcopy(CHEAT_CODE_2)
+                # CHEAT_CODE = make_cheat_code(alliance)
+                # CHEAT_CODE_1, CHEAT_CODE_2 = CHEAT_CODE[:5], CHEAT_CODE[5:]
+                # CHEAT_CODE_COPY_1, CHEAT_CODE_COPY_2 = copy.deepcopy(CHEAT_CODE_1), copy.deepcopy(CHEAT_CODE_2)
 
             if message[1] == 'button_press':
                 PRESSED_ID = int(message[2]['id'])
@@ -161,32 +161,32 @@ def whack_a_mole_start(alliance):
                 if PRESSED_ID == button:
                     correct_pressed = True
 
-                if check_live_coding(alliance):
-                    if len(CHEAT_CODE_1) > 0 or len(CHEAT_CODE_2) > 0:
-                        if PRESSED_ID == CHEAT_CODE_1[0]:
-                            print("Cheat code 1 pop: ", CHEAT_CODE_1[0])
-                            CHEAT_CODE_1.pop(0)
-                            print("Cheat code 1: ", CHEAT_CODE)
-                            cheat_code_pressed = (len(CHEAT_CODE_1) == 0)
-                        else:
-                            print("Reset cheat_code 1")
-                            # reset cheat_code, if cheat code is not done in order
-                            CHEAT_CODE_1 = CHEAT_CODE_COPY_1
-                            CHEAT_CODE_COPY_1 = copy.deepcopy(CHEAT_CODE_COPY_1)
-                            print(CHEAT_CODE_1)
+                # if check_live_coding(alliance):
+                #     if len(CHEAT_CODE_1) > 0 or len(CHEAT_CODE_2) > 0:
+                #         if PRESSED_ID == CHEAT_CODE_1[0]:
+                #             print("Cheat code 1 pop: ", CHEAT_CODE_1[0])
+                #             CHEAT_CODE_1.pop(0)
+                #             print("Cheat code 1: ", CHEAT_CODE)
+                #             cheat_code_pressed = (len(CHEAT_CODE_1) == 0)
+                #         else:
+                #             print("Reset cheat_code 1")
+                #             # reset cheat_code, if cheat code is not done in order
+                #             CHEAT_CODE_1 = CHEAT_CODE_COPY_1
+                #             CHEAT_CODE_COPY_1 = copy.deepcopy(CHEAT_CODE_COPY_1)
+                #             print(CHEAT_CODE_1)
 
                             
-                        if PRESSED_ID == CHEAT_CODE_2[0]:
-                            print("Cheat code pop 2: ", CHEAT_CODE_2[0])
-                            CHEAT_CODE_2.pop(0)
-                            print("Cheat code 2: ", CHEAT_CODE)
-                            cheat_code_pressed = (len(CHEAT_CODE_2) == 0)
-                        else:
-                            print("Reset cheat_code 2")
-                            # reset cheat_code, if cheat code is not done in order
-                            CHEAT_CODE_2 = CHEAT_CODE_COPY_2
-                            CHEAT_CODE_COPY_2 = copy.deepcopy(CHEAT_CODE_COPY_2)
-                            print(CHEAT_CODE_2)
+                #         if PRESSED_ID == CHEAT_CODE_2[0]:
+                #             print("Cheat code pop 2: ", CHEAT_CODE_2[0])
+                #             CHEAT_CODE_2.pop(0)
+                #             print("Cheat code 2: ", CHEAT_CODE)
+                #             cheat_code_pressed = (len(CHEAT_CODE_2) == 0)
+                #         else:
+                #             print("Reset cheat_code 2")
+                #             # reset cheat_code, if cheat code is not done in order
+                #             CHEAT_CODE_2 = CHEAT_CODE_COPY_2
+                #             CHEAT_CODE_COPY_2 = copy.deepcopy(CHEAT_CODE_COPY_2)
+                #             print(CHEAT_CODE_2)
 
         if correct_pressed:
             mole_press_count += 1
@@ -196,11 +196,11 @@ def whack_a_mole_start(alliance):
             time.sleep(0.2)
             turn_all_lights(alliance, on=False)
 
-        if cheat_code_pressed:
-            CHEAT_CODE_DONE += 1
-            cheat_code_pressed = False
-            send_cheat_code_score(alliance, CHEAT_CODE_DONE)
-            celebrate(alliance)
+        # if cheat_code_pressed:
+        #     CHEAT_CODE_DONE += 1
+        #     cheat_code_pressed = False
+        #     send_cheat_code_score(alliance, CHEAT_CODE_DONE)
+        #     celebrate(alliance)
 
         if mole_press_count == REQUIREMENT:
             mole_press_count += 1
@@ -209,12 +209,79 @@ def whack_a_mole_start(alliance):
             send_security_breach_score(alliance, MOLE_PRESS_DONE)
 
         turn_off_light(button)
-        send_cheat_code_score(alliance, CHEAT_CODE_DONE)
+       #send_cheat_code_score(alliance, CHEAT_CODE_DONE)
         send_security_breach_score(alliance, MOLE_PRESS_DONE)
 
 
-threading.Thread(target=whack_a_mole_start, args=(
-    ALLIANCE_COLOR.BLUE,), daemon=True).start()
-threading.Thread(target=whack_a_mole_start, args=(
-    ALLIANCE_COLOR.GOLD,), daemon=True).start()
+def sail_task(allience):
+    turn_all_lights(allience, on=False)
+
+    REQUIREMENT_FOR_SAIL = 5
+    SAIL_TIMEOUT = 15
+    correct_presses = 0
+    last_press_time = time.time()
+
+    if allience == ALLIANCE_COLOR.BLUE:
+        EVENT_QUEUE = BLUE_QUEUE 
+    else:
+        EVENT_QUEUE = GOLD_QUEUE
+    while True: #this lights up a random button
+        if allience == ALLIANCE_COLOR.BlUE:
+            button = int(random.random() * NUM_BUTTONS)
+        else:
+            button = int(random.random() * NUM_BUTTONS) + NUM_BUTTONS
+        turn_on_light(button)
+
+        start_time = time.time() # wait for the button press or timeout???
+        correct_presses = False
+    
+        while time.time() - start_time < SAIL_TIMEOUT:
+            try:
+                message = EVENT_QUEUE.get(timeout = 0.01)
+            except queue.Empty:
+                continue
+            if message[1] == 'button_press':
+                    PRESSED_ID = int(message[2]['id'])
+                    print("Button pressed: ", PRESSED_ID)
+                    turn_on_light(PRESSED_ID)
+                    time.sleep(0.2)
+                    turn_off_light(PRESSED_ID)
+                    
+                    if PRESSED_ID == button:
+                        correct_pressed = True
+                        break  # exit the loop if the correct button is pressed
+                    else:
+                        # incorrect button pressed, reset the process
+                        correct_presses = 0
+                        turn_all_lights(allience, on=False)
+                        print("Incorrect button pressed! Resetting SAIL task.")
+                        break
+    
+        # Check if the correct button was pressed within the timeout
+        if correct_pressed:
+                correct_presses += 1
+                last_press_time = time.time()
+                print(f"Correct presses: {correct_presses}")
+                if correct_presses == REQUIREMENT_FOR_SAIL:
+                    print("SAIL fully hoisted! Bonus points awarded.")
+                    celebrate(allience)
+                    send_security_breach_score(allience, True)  # Send bonus points
+                    correct_presses = 0  # Reset for the next attempt
+        else:
+                # Timeout occurred, reset the process
+                correct_presses = 0
+                turn_all_lights(allience, on=False)
+                print("Timeout! Resetting SAIL task.")
+
+        turn_off_light(button)
+
+
+
+
+
+threading.Thread(target=whack_a_mole_start, args=(ALLIANCE_COLOR.BLUE,), daemon=True).start()
+threading.Thread(target=whack_a_mole_start, args=(ALLIANCE_COLOR.GOLD,), daemon=True).start()
+threading.Thread(target=sail_task, args=(ALLIANCE_COLOR.BLUE,), daemon=True).start()
+threading.Thread(target=sail_task, args=(ALLIANCE_COLOR.GOLD,), daemon=True).start()
+
 fill_queue()
