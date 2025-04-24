c201456bc9f3bd3e7c8d89b9c82072bd7e24adcb
n1qqq <n1qqq@foxmail.com>
2025-04-23 20:15:44 -0700

    Removed match statement

--- src/sheet.py
+++ src/sheet.py
@@ -373,17 +373,15 @@ class Sheet:
         if row_num == -1:
             print("Error: row number not found!")
             return
-        match team:                                 # Python match clause doesn't fall!
-            case INDICES.BLUE_1:
-                range_name = f"Ref!E{row_num + 4}"
-            case INDICES.BLUE_2:
-                range_name = f"Ref!F{row_num + 4}"
-            case INDICES.GOLD_1:
-                range_name = f"Ref!E{row_num + 5}"
-            case INDICES.GOLD_2:
-                range_name = f"Ref!F{row_num + 5}"
-            case _:
-                print("Error: unknown team id!")
+        if team == INDICES.BLUE_1:
+            range_name = f"Ref!E{row_num + 4}"
+        elif team == INDICES.BLUE_2:
+            range_name = f"Ref!F{row_num + 4}"
+        elif team == INDICES.GOLD_1:
+            range_name = f"Ref!E{row_num + 5}"
+        elif team == INDICES.GOLD_2:
+            range_name = f"Ref!F{row_num + 5}"
+        else: print("Error: unknown team id!")
         body = {
             'values': [[int(score)]]
         }
