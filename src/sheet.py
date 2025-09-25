c0e373cffde70baa68887771dbe5ac6ec8249091
n1qqq <n1qqq@foxmail.com>
2025-09-25 14:27:47 +0800

    Add Documentation for Google Sheets

--- src/sheet.py
+++ src/sheet.py
@@ -9,6 +9,8 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
+from googleapiclient.errors import HttpError
+
 from ydl import Client
 from utils import *
 
@@ -421,6 +423,7 @@ class Sheet:
         elif alliance == ALLIANCE_COLOR.GOLD:
             range_name = f"Ref!Z{row_num + 5}"
         else:
+            range_name = ""
             print("Alliance does not exist!")
 
         body = {
@@ -430,3 +433,51 @@ class Sheet:
         spreadsheet.values().update(spreadsheetId=CONSTANTS.SPREADSHEET_ID,
                             range=range_name, body=body, valueInputOption="RAW").execute()
         print("Sail checked in Sheets")
+
+
+def main():
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists(USER_TOKEN_FILE):
+        creds = Credentials.from_authorized_user_file(USER_TOKEN_FILE, SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                CLIENT_SECRET_FILE, SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open(USER_TOKEN_FILE, "w") as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build("sheets", "v4", credentials=creds)
+
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = (
+            sheet.values()
+            .get(spreadsheetId=CONSTANTS.SPREADSHEET_ID, range="Match Database!A1:B")
+            .execute()
+        )
+        values = result.get("values", [])
+
+        if not values:
+            print("No data found.")
+            return
+
+        print("Success.")
+    except HttpError as err:
+        print(err)
+
+
+if __name__ == "__main__":
+    main()
